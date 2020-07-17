02e7a9bde3a9c43bb9e1fd88e2e49ec15877a942
Davidoumone <david.moneta28@gmail.com>
2020-07-17 09:13:30 +0200

    formulaire réalisé

--- front/homerodyssey/src/components/SignUp.jsx
+++ front/homerodyssey/src/components/SignUp.jsx
@@ -1,16 +1,69 @@
 import React, { useState } from "react";
+import "./SignUp.css";
 
 function SignUp() {
   const [email, updateEmailField] = useState("");
+  const [password, updatePassword] = useState("");
+  const [name, updateName] = useState("");
+  const [lastname, updateLastName] = useState("");
+
+  function handleSubmit() {
+    console.log(
+      "email : " +
+        email +
+        ", password : " +
+        password +
+        ", name : " +
+        name +
+        ", lastname : " +
+        lastname
+    );
+  }
+
   return (
-    <div>
-      <h1>{email}</h1>
-      <input
-        type="email"
-        name="email"
-        onChange={(event) => updateEmailField(event.target.value)}
-      />
-    </div>
+      <form onSubmit={() => handleSubmit()} className="formulaire">
+        <h1>
+          {"email : " +
+            email +
+            ", password : " +
+            password +
+            ", name : " +
+            name +
+            ", lastname : " +
+            lastname}
+        </h1>
+        <input
+          type="email"
+          name="email"
+          placeholder="enter example@gmail.com"
+          onChange={(event) => updateEmailField(event.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="enter password"
+          onChange={(event) => updatePassword(event.target.value)}
+        />
+        <input
+          type="password"
+          name="passwordbis"
+          placeholder=" enter password again"
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="enter your name"
+          onChange={(event) => updateName(event.target.value)}
+        />
+        <input
+          type="text"
+          name="lastname"
+          placeholder="enter your last name"
+          onChange={(event) => updateLastName(event.target.value)}
+        />
+        <input type="submit" value="Soumettre" />
+      </form>
+    
   );
 }
 
